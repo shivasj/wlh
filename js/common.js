e2d4fe28b36c55882eadf98b01807b0675911b92
Shivas Jayaram <shivasj@gmail.com>
2016-05-10 07:42:24 -0400

    Project files commit

---
+++ js/common.js
@@ -0,0 +1,39 @@
+// Load Header and Footer template
+$(function() {
+
+    $('#header').load('templates/header.html', function() {
+
+        var page = "index.html";
+
+        var match = document.location.pathname.match(/[^\/]+$/);
+        if(match){
+            page = match[0];
+        }
+        //console.log(page);
+
+        switch(page) {
+            case "index.html":
+                $('#index_page_menu').addClass("active");
+                break;
+            case "items.html":
+                $('#items_page_menu').addClass("active");
+                break;
+            case "register.html":
+                $('#register_page_menu').addClass("active");
+                break;
+            case "contact.html":
+                $('#contact_page_menu').addClass("active");
+                break;
+            case "signin.html":
+                $('#signin_page_menu').addClass("active");
+                break;
+            default:
+                $('#index_page_menu').addClass("active");
+        }
+
+    });
+
+    $('#footer').load('templates/footer.html', function() {});
+
+
+});
